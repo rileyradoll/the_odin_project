e39bf8e27dd24e462aca3fb29cdb25dbadcdf657
Riley Radoll <rileyradoll@gmail.com>
2019-07-26 23:15:57 -0400

    Leveling up my ruby skills

---
+++ learn_to_program/calc.rb
@@ -0,0 +1,8 @@
+"how many hours are in a year?"
+puts 24 * 365
+"how many minutes are in a decade?"
+puts 10 * 24 * 60 * 365
+"how many seconds old are you?"
+puts 60 * 60 * 24 * 365 * 29
+"If I am 1298 million seconds old, how old am I?" 
+puts 1298000000 / 60 / 60 / 24 / 365
